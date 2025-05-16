a12fb22b1f32b500f98c34bf5a0f878d93311dce
Luis Angel <angelfgdeveloper@gmail.com>
2025-05-16 16:01:26 -0600

    Obtener pokemon por id

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Pokemon } from '@/pokemons';
 
 interface Props {
   params: Promise<{
@@ -5,13 +6,28 @@ interface Props {
   }>;
 }
 
+const getPokemon = async (id: string): Promise<Pokemon> => {
+
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ id }`, {
+    cache: 'force-cache'
+  }).then(resp => resp.json());
+
+  console.log('Se cargo:', pokemon.name);
+
+  return pokemon;
+}
+
 export default async function PokemonPage({ params }: Props) {
 
   const { id } = await params;
+  const pokemon = await getPokemon(id);
   
   return (
     <div>
       <h1>Pokemon { id }</h1>
+      <div>
+        { JSON.stringify(pokemon) }
+      </div>
     </div>
   );
 }
